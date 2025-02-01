61089b69d3a0a262fc191609eeca46bc701235ae
zakis <zakisgm@gmail.com>
2025-02-02 00:13:38 +0200

    static files

--- superfoods/views.py
+++ superfoods/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.template.defaulttags import comment
+
 from .models import  Products,Categories,Manufacturers
 from django.utils import timezone
+
 
 
 # Create your views here.
 def post_list(request):
     products = Products.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'products':products})
+
+
+def post_detail(request, pk):
+    products_detail = get_object_or_404(Products, pk=pk)
+    return render(request, 'blog/post_detail.html', {'products_detail': products_detail})
